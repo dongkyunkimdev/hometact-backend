18c9591634ed78f194dc2666b91ead9b8b44c90c
dongkyunkimdev <ddkds66@gmail.com>
2022-02-16 00:07:11 +0900

    test: PostLike  Repository 테스트 케이스 추가

--- src/test/java/kdk/hometact/postlike/PostLikeRepositoryTest.java
+++ src/test/java/kdk/hometact/postlike/PostLikeRepositoryTest.java
@@ -1,7 +1,9 @@
 package kdk.hometact.postlike;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collections;
 import kdk.hometact.error.ErrorCode;
@@ -11,7 +13,6 @@ import kdk.hometact.post.PostRepository;
 import kdk.hometact.user.User;
 import kdk.hometact.user.UserRepository;
 import kdk.hometact.user.auth.Authority;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -70,6 +71,51 @@ class PostLikeRepositoryTest {
 
 		// then
 		assertThat(e.getMessage()).isEqualTo(ErrorCode.ENTITY_NOT_FOUND.getMessage());
+	}
+
+	@Test
+	void 유저와_게시글로_좋아요_조회() {
+		// given
+		String email = "test1@test.com";
+		User user = userRepository.save(createUser(email, "password", "test1"));
+		Post post = postRepository.save(createPost("title", "content", email));
+		PostLike postLike = postLikeRepository.save(createPostLike(user, post));
+
+		// when
+		PostLike result = postLikeRepository.findByPostAndUser(post, user).orElse(null);
+
+		// then
+		assertThat(user).isEqualTo(result.getUser());
+		assertThat(post).isEqualTo(result.getPost());
+	}
+
+	@Test
+	void 유저와_게시글로_좋아요_조회_존재_TRUE() {
+		// given
+		String email = "test1@test.com";
+		User user = userRepository.save(createUser(email, "password", "test1"));
+		Post post = postRepository.save(createPost("title", "content", email));
+		PostLike postLike = postLikeRepository.save(createPostLike(user, post));
+
+		// when
+		boolean result = postLikeRepository.existsByPostAndUser(post, user);
+
+		// then
+		assertTrue(result);
+	}
+
+	@Test
+	void 유저와_게시글로_좋아요_조회_존재_FALSE() {
+		// given
+		String email = "test1@test.com";
+		User user = userRepository.save(createUser(email, "password", "test1"));
+		Post post = postRepository.save(createPost("title", "content", email));
+
+		// when
+		boolean result = postLikeRepository.existsByPostAndUser(post, user);
+
+		// then
+		assertFalse(result);
 	}
 
 	private User createUser(String email, String password, String nickname) {
